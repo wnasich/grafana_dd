72155712f7e8cbc928f034da7de2296ad0ec3662
zhil <starsfm@gmail.com>
2019-07-16 22:29:52 +0300

    fixed bug with cyrillic transliteration

--- utils.php
+++ utils.php
@@ -8,18 +8,3 @@ $client = new Client([
 		'Authorization' => 'Bearer ' . $config['apiKey']
     ],
 ]);
-
-function slugify($text) {
-	$text = preg_replace('~[^\pL\d]+~u', '-', $text);
-	$text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
-	$text = preg_replace('~[^-\w]+~', '', $text);
-	$text = trim($text, '-');
-	$text = preg_replace('~-+~', '-', $text);
-	$text = strtolower($text);
-
-	if (empty($text)) {
-		return 'n-a';
-	}
-
-	return $text;
-}

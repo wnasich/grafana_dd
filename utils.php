eb044f9468a4f4798547ac556a1f623e3c3bc808
Walter Nasich <wnasich@gmail.com>
2020-01-28 11:35:05 -0300

    Using a longer timeout

--- utils.php
+++ utils.php
@@ -3,7 +3,7 @@ use GuzzleHttp\Client;
 
 $client = new Client([
     'base_uri' => $config['baseUri'],
-    'timeout'  => 2.0,
+    'timeout'  => 20,
     'headers' => [
 		'Authorization' => 'Bearer ' . $config['apiKey']
     ],
